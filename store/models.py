b794428e44a5786c4986b1d900e3d7a14bcd3a45
martinsllm <lucaslm2010@gmail.com>
2025-04-18 18:47:00 -0300

    Add tables category, product and images

--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    categoria = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField(
+        blank=False, null=False, default=0)
+    preco_compra = models.FloatField()
+    preco_venda = models.FloatField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="imgs")
+    produto = models.ForeignKey(Product, on_delete=models.CASCADE)
