86c44860e6cd23c774cf656482f5a1ed9107cdfa
martinsllm <lucaslm2010@gmail.com>
2025-04-20 19:25:30 -0300

    Updating products

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from store.views.ProductListView import ProductListView
-from store.views.ProductView import ProductView
+from store.views.ProductUpdateView import ProductUpdateView
 from store.views.ProductCreateView import ProductCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('add_product/', ProductCreateView.as_view(), name='add_product'),
-    path('product/<slug:slug>', ProductView.as_view(), name='product'),
+    path('product/<slug:slug>', ProductUpdateView.as_view(), name='product_update'),
 ]
--- store/views/ProductUpdateView.py
+++ store/views/ProductUpdateView.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from store.models import Product
 from store.forms import ProductForm
 
 
-class ProductView(LoginRequiredMixin, View):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
+    fields = '__all__'
+    success_url = reverse_lazy("product_list")
 
     def get(self, request, slug):
         product = Product.objects.get(slug=slug)
